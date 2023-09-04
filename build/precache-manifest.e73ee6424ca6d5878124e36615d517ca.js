4f03cdf3f9b25b8f63a3fd138d03a0fe7b10bea8
Karthikeyan <66964518+dr-poco@users.noreply.github.com>
2023-09-04 20:11:26 +0530

    Add files via upload

---
+++ build/precache-manifest.e73ee6424ca6d5878124e36615d517ca.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f348cfb708d13aa3e449fac16c692faa",
+    "url": "/The_Madras_App/index.html"
+  },
+  {
+    "revision": "1c540fe9a9b8c965f079",
+    "url": "/The_Madras_App/static/js/2.f083c6dd.chunk.js"
+  },
+  {
+    "revision": "bbecb32ffb804ff1c292",
+    "url": "/The_Madras_App/static/js/main.74e1d5f5.chunk.js"
+  },
+  {
+    "revision": "960459ae1171a5e477f4",
+    "url": "/The_Madras_App/static/js/runtime-main.e72f850c.js"
+  }
+]);
